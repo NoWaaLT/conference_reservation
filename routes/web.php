9bc8f4be9e02ea86e053193c6f30f60533a61ffe
NoWaaLT <vytautas.jelisejevas@gmail.com>
2024-11-18 21:52:53 +0200

    Added roles;

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ClientController;
-use App\Http\Controllers\EmployeeController;
-use App\Http\Controllers\admin\ConferenceController;
-use App\Http\Controllers\admin\UserController;
 
 Route::get('/', function () {
-    return view('homepage');
+    return view('welcome');
 });
 
-Route::get('/register/{id}', function (int $conferenceId) {
-    return view('register', ['conference' => $conferenceId]);
-})->name('regiter.form');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::post('register/{conference}', [ClientController::class, 'submit'])->name('register.submit');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::resource('clients', ClientController::class);
-Route::resource('employee', EmployeeController::class);
-Route::resource('users', UserController::class);
-Route::resource('conferences', ConferenceController::class);
+require __DIR__.'/auth.php';
